55b9c0bfe16bb2c45c5adc90cc901926582031df
ndrzmansn <ndrzmansn@gmail.com>
2010-02-02 11:13:55 +0100

    deleted some irrelavent files Please enter the commit message for your changes. Lines starting

--- lab5/particles_simple/simple_kernel.cu
+++
@@ -1,9 +0,0 @@
-
-__global__ void simple_kernel(float4* pos, float4* vels, unsigned int width, unsigned int height, float time, float rand, float prand)
-{
-    // Indices into the VBO data. Roughly like texture coordinates from GLSL.
-    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
-    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
-
-    // TODO :: Write some value to pos[y*width + x], and make a particle system!
-}
